d5daeef15e3dd95df6a5b0fc5fa1c27d27850958
vzjuunmmx <128061099+vzjuunmmx@users.noreply.github.com>
2024-04-05 12:19:29 -0300

    Add files via upload

--- projeto de vida-julia/main.js
+++ projeto de vida-julia/main.js
@@ -1,16 +1,23 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo")
+
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
         
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
-        }
+            botoes[i].classList.add("ativo");
+         }
         
-        botoes[i].classList.add("ativo");
-        
+            botoes[j].classList.add("ativo");
+            botoes[i].classList.add("ativo");
     }
-
 }
+
+const contadores = document.querySelectorAll(".contador"); 
+const tempoObjetivo1 = new Date("2024-10-05"); 
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempo0bjetivo1; 
 
 
